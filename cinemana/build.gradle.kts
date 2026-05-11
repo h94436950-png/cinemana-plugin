5bf7f9456128aa471c2b6ace819685b1667697d1
h94436950-png <h94436950@gmail.com>
2026-05-11 12:36:55 +0300

    Update build.gradle.kts

--- cinemana/build.gradle.kts
+++ cinemana/build.gradle.kts
@@ -5,25 +5,32 @@ cloudstream {
     language = "ar"
     authors = listOf("okibcn")
     status = 1
+
     tvTypes = listOf(
         "Movie",
         "TvSeries",
     )
-    // iconUrl = "https://..."
 }
 
 android {
     namespace = "com.cinemana"
+
     buildFeatures {
         buildConfig = true
     }
-    kotlinOptions {
-        freeCompilerArgs += "-XXLanguage:+BreakContinueInInlineLambdas"
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.add(
+            "-XXLanguage:+BreakContinueInInlineLambdas"
+        )
     }
 }
 
 dependencies {
     val implementation by configurations
+
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.browser:browser:1.9.0")
     implementation("androidx.room:room-ktx:2.8.0")
